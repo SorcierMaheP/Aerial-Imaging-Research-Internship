f4b242f27381fe73742c5faadab7bcfa32ba5c58
QuickAxe <106764539+ManselInfinity@users.noreply.github.com>
2024-07-31 22:41:39 +0530

    fixed validate code

--- val.py
+++ val.py
@@ -1,12 +1,14 @@
 from ultralytics import YOLO
 
-# Load a model
-# model = YOLO("yolov8n.pt")  # load an official model
-model = YOLO("Models/yolov10s_last_100.pt")  # load a custom model
+if __name__ == "__main__":
+    # Load a model
+    # model = YOLO("yolov8n.pt")  # load an official model
+    model = YOLO("Models/yolov8n_improvedECA_best_ncnn_model")  # load a custom model
 
-# Validate the model
-metrics = model.val(split="test")  # no arguments needed, dataset and settings remembered
-metrics.box.map  # map50-95
-metrics.box.map50  # map50
-metrics.box.map75  # map75
-metrics.box.maps  # a list contains map50-95 of each category
+    # Validate the model
+    metrics = model.val(data="C:/Users/PC/Downloads/augmented_dataset/Trspeclass.v2i.yolov8/data.yaml",
+                        split="test")  # no arguments needed, dataset and settings remembered
+    metrics.box.map  # map50-95
+    metrics.box.map50  # map50
+    metrics.box.map75  # map75
+    metrics.box.maps  # a list contains map50-95 of each category
